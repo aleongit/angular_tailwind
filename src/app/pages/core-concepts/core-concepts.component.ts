4b7fdfa862f66f694786b6c50fd50e9e64549fba
aleongit <aleix.leon@gmail.com>
2024-05-08 17:41:25 +0200

    wip: core concepts

--- src/app/pages/core-concepts/core-concepts.component.ts
+++ src/app/pages/core-concepts/core-concepts.component.ts
@@ -17,7 +17,7 @@ export class CoreConceptsComponent implements OnInit, AfterViewChecked {
 
   code = `<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
   <div class="shrink-0">
-    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+    <img class="h-12 w-12" src="${this.constants.ICON_MENU}" alt="ChitChat Logo">
   </div>
   <div>
     <div class="text-xl font-medium text-black">
