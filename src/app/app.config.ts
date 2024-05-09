9821b673d61405eb0ce179b96f1f875725089acc
aleongit <aleix.leon@gmail.com>
2024-05-09 15:25:37 +0200

    chore: activate anchorScrolling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
 import { routes } from './app.routes';
+import {
+  InMemoryScrollingOptions,
+  InMemoryScrollingFeature,
+  withInMemoryScrolling,
+} from '@angular/router';
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [
+    provideRouter(routes, inMemoryScrollingFeature),
+    provideAnimations(),
+  ],
 };
