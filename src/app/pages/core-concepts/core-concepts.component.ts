1a8e32c56326c2e5e17c14aad42fc5392531d591
aleongit <aleix.leon@gmail.com>
2024-05-10 11:59:13 +0200

    feat: add section responsive to core concepts

--- src/app/pages/core-concepts/core-concepts.component.ts
+++ src/app/pages/core-concepts/core-concepts.component.ts
@@ -17,7 +17,7 @@ export class CoreConceptsComponent implements OnInit, AfterViewChecked {
   constructor(
     private highlightService: HighlightService,
     private sanitized: DomSanitizer
-  ) { }
+  ) {}
 
   code1 = `<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
   <div class="shrink-0">
@@ -33,10 +33,24 @@ export class CoreConceptsComponent implements OnInit, AfterViewChecked {
 
   code2 = `<button class="bg-sky-500 hover:bg-sky-700">
 Save changes
-</button>`
-  code2Sanitized = this.sanitized.bypassSecurityTrustHtml(`<button class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white">
+</button>`;
+  code2Sanitized = this.sanitized
+    .bypassSecurityTrustHtml(`<button class="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold text-white">
   Save changes
 </button>`);
+
+  code3 = `<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+<div class="md:flex">
+  <div class="md:shrink-0">
+    <img class="h-48 w-full object-cover md:h-full md:w-48" src="${this.constants.ICON_MENU}" alt="Modern building architecture">
+  </div>
+  <div class="p-8">
+    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+    <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+  </div>
+</div>
+</div>`;
 
   /**
    * Lifecycle hook AfterViewChecked is used here to call the highlight service
@@ -55,5 +69,5 @@ Save changes
   /**
    * Fetch blog post from API
    */
-  ngOnInit() { }
+  ngOnInit() {}
 }
