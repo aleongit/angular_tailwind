c3655695b9644b49b676747532574ee3ea93a684
aleongit <aleix.leon@gmail.com>
2024-05-13 13:46:31 +0200

    feat: customization page added

--- src/app/pages/customization/customization.component.ts
+++ src/app/pages/customization/customization.component.ts
@@ -56,4 +56,58 @@ export class CustomizationComponent implements AfterViewChecked {
       }
     },
   }`;
+
+  code2 = `/** @type {import('tailwindcss').Config} */
+  module.exports = {
+    content: [
+      './pages/**/*.{html,js}',
+      './components/**/*.{html,js}',
+    ],
+    // ...
+  }`;
+
+  code3 = `/** @type {import('tailwindcss').Config} */
+  module.exports = {
+    theme: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    }
+  }`;
+
+  code4 = `/** @type {import('tailwindcss').Config} */
+  module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'tahiti': {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+        },
+        // ...
+      },
+    },
+  }`;
 }
